d821501feede32bce9cd6ec39ca42243bfe5e57f
Christophe Levis <christophe.levis@sonarsource.com>
2016-05-17 13:51:48 +0200

    Don't change sonarqube config file on restart

--- sonarqube/bin/s-sonar.sh
+++ sonarqube/bin/s-sonar.sh
@@ -17,40 +17,37 @@ then
   s-clearDataFolder.sh
 
   # print out properties for the correct DB
-  if [ "$2" = "P" ] 
-  then
-    echo "sonar.jdbc.url=$P_JDBC_URL" >> $SONAR_PROPERTIES_FILE
-    setDefaultCredentials
-  else
-    if [ "$2" = "M" ] 
-    then
-      echo "sonar.jdbc.url=$M_JDBC_URL" >> $SONAR_PROPERTIES_FILE
-    setDefaultCredentials
-    else
+  #if [ "$2" = "P" ] 
+  #then
+  #  echo "sonar.jdbc.url=$P_JDBC_URL" >> $SONAR_PROPERTIES_FILE
+  #else
+  #  if [ "$2" = "M" ] 
+  #  then
+  #    echo "sonar.jdbc.url=$M_JDBC_URL" >> $SONAR_PROPERTIES_FILE
+  #  else
       if [ "$2" = "O" ] 
       then
         # copy the driver if it does not exist
-        ORACLE_DRIVER="$SONAR_NEXT/extensions/jdbc-driver/oracle/ojdbc6-11.2.0.3.0.jar"
+        ORACLE_DRIVER="$SONAR_NEXT/extensions/jdbc-driver/oracle/ojdbc6.jar"
         if [ ! -f "$ORACLE_DRIVER" ]
         then
           echo "Copying ORACLE driver"
-          cp $SOFTWARE_FOLDER/SonarQube/ojdbc6-11.2.0.3.0.jar $SONAR_CURRENT/extensions/jdbc-driver/oracle/
+          cp $SOFTWARE_FOLDER/SonarQube/oracle/ojdbc6.jar $SONAR_CURRENT/extensions/jdbc-driver/oracle/
         fi
 
-        echo "sonar.jdbc.url=$O_JDBC_URL" >> $SONAR_PROPERTIES_FILE
-        setDefaultCredentials
-      else
-        if [ "$2" = "MS" ] 
-        then
-          echo "sonar.jdbc.url=$MS_JDBC_URL" >> $SONAR_PROPERTIES_FILE
-          setDefaultCredentials
-        else
-          echo "sonar.jdbc.url=$H_JDBC_URL" >> $SONAR_PROPERTIES_FILE
-          echo "sonar.embeddedDatabase.port=9092" >> $SONAR_PROPERTIES_FILE
-        fi
+  #      echo "sonar.jdbc.url=$O_JDBC_URL" >> $SONAR_PROPERTIES_FILE
+  #    else
+  #      if [ "$2" = "MS" ] 
+  #      then
+  #        echo "sonar.jdbc.url=$MS_JDBC_URL" >> $SONAR_PROPERTIES_FILE
+  #      else
+  #        echo "sonar.jdbc.url=$H_JDBC_URL" >> $SONAR_PROPERTIES_FILE
+  #        echo "sonar.embeddedDatabase.port=9092" >> $SONAR_PROPERTIES_FILE
+  #      fi
       fi
-    fi
-  fi
+  #  fi
+  #fi
+  #setDefaultCredentials
 fi
 
 $SONAR_CURRENT/bin/$SONAR_WRAPPER_FOLDER/sonar.sh $1
