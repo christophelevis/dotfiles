3d41f377ccd32f8027b43ae64304707cb622a648
Christophe Levis <christophe.levis@sonarsource.com>
2016-06-02 10:32:45 +0200

    Update with 'snapshot' and usage

--- sonarqube/bin/s-switch.sh
+++ sonarqube/bin/s-switch.sh
@@ -1,30 +1,42 @@
 #!/bin/sh
 
-s-sonar.sh stop
+usage() {
+    echo "Usage: `basename $0` [version]"
+    echo "    where version can be 'snapshot', 'X.Y', 'X.Y-RC*', 'X.Y-build*...'"
+    exit 0
+}
 
-if [ "$1" = "" ] 
+CURRENT=""
+case "$1" in 
+    "")
+        echo "Current version is unchanged"
+        usage
+        ;;
+    "snapshot")
+        VERSION="SNAPSHOT"
+        CURRENT=$SONAR_NEXT
+        ;;
+    *)
+        if [ -d $SOFTWARE_FOLDER/SonarQube/sonarqube-$1 ] 
+        then
+            VERSION="$1"
+            CURRENT=$SOFTWARE_FOLDER/SonarQube/sonarqube-$1
+        elif [ -d $SOFTWARE_FOLDER/SonarQube/sonar-$1 ] 
+        then
+            VERSION="$1"
+            CURRENT=$SOFTWARE_FOLDER/SonarQube/sonar-$1
+        else
+            echo "No SQ install folder found for version $1"
+            echo "Current version is unchanged"
+            usage
+        fi
+        ;;
+esac
+
+if [ -n "$VERSION" ]
 then
-  CURRENT=$SONAR_NEXT
-  echo "SonarQube SNAPSHOT configured as current installation."
-else
-  if [ -d $SOFTWARE_FOLDER/SonarQube/sonarqube-$1 ] 
-  then
-    CURRENT=$SOFTWARE_FOLDER/SonarQube/sonarqube-$1
-    echo "SonarQube $1 configured as current installation."
-  elif [ -d $SOFTWARE_FOLDER/SonarQube/sonar-$1 ] 
-  then
-  	CURRENT=$SOFTWARE_FOLDER/SonarQube/sonar-$1
-  	echo "Sonar $1 configured as current installation."
-  else
-  	echo "No SQ install folder found for version $1"
-  	echo "Current version is unchanged"
-  fi
+    s-sonar.sh stop
+    rm $SONAR_CURRENT
+    ln -s $CURRENT $SONAR_CURRENT
+    echo "SonarQube $VERSION configured as current installation"
 fi
-
-if [ "$CURRENT" != "" ]
-then
-	rm $SONAR_CURRENT
-	ln -s $CURRENT $SONAR_CURRENT
-fi
-
-s-sonar.sh start
