f4b343f8e2781a49ab303cba90e5deab71b78ce1
Christophe Levis <christophe.levis@sonarsource.com>
2016-06-06 13:46:12 +0200

    Refactor plugin install

--- sonarqube/bin/s-installSnapshot.sh
+++ sonarqube/bin/s-installSnapshot.sh
@@ -3,20 +3,20 @@
 # Exit on failure
 set -e
 
+case "$1" in 
+    -h|--h|*-help)
+        echo "Usage: `basename $0` ..."
+	s-installSnapshotOfPlugin.sh -help
+        exit 0
+        ;;
+esac
+
+echo "Install snapshot of sonarqube"
+
 s-sonar.sh stop
 
-# Check if build is required for plugins
-for var in "$@"
-do
-    if [ "$var" = "-o" ]
-    then
-        echo "No build required for plugins"
-        PLUGIN_BUILD_OPTION="-o"
-    fi
-done
-
-# Get latest version of SonarQube
-echo "Get latest version of SonarQube"
+# Get latest version of sonarqube
+echo "Get latest version"
 cd $REPOS/sonarqube
 git pull
 
@@ -25,8 +25,8 @@ echo "Build (quiet mode)..."
 ./quick-build.sh -q
 
 # Unzip built package
-echo "Extract distribution"
 SONAR_VERSION=`ls $REPOS/sonarqube/sonar-application/target/sonarqube-*-SNAPSHOT.zip | sed 's/.*target\/sonarqube-\(.*\)-SNAPSHOT\.zip/\1/' `
+echo "Extract sonarqube version '$SONAR_VERSION'"
 
 rm -rf $SONAR_NEXT_FILES/INSTALL/*
 unzip -q $REPOS/sonarqube/sonar-application/target/sonarqube-$SONAR_VERSION-SNAPSHOT.zip -d $SONAR_NEXT_FILES/INSTALL
@@ -40,7 +40,7 @@ rm $SONAR_CURRENT
 ln -s $SONAR_NEXT_FILES/INSTALL/sonarqube-$SONAR_VERSION-SNAPSHOT $SONAR_CURRENT
 
 # Configure dev update center
-echo "Configure SonarQube with dev update center"
+echo "Configure sonarqube with dev update center"
 echo "\n\nsonar.updatecenter.url=http://update.sonarsource.org/update-center-dev.properties" >> $SONAR_NEXT/conf/sonar.properties
 
 # Restore backup plugins
@@ -50,14 +50,9 @@ then
     cp $SONAR_NEXT_FILES/BACKUP/*.jar $SONAR_NEXT/extensions/plugins/
 fi
  
-# Checks if plugins must be installed or not
-for var in "$@"
-do
-    if [ "$var" != "-o" ]
-    then
-        echo "Install latest version of plugin '$var'"
-        s-installLatestVersionOfPlugin.sh $var $PLUGIN_BUILD_OPTION
-    fi
-done
+if [ -n "$1" ]
+then
+    s-installSnapshotOfPlugin.sh $1
+fi
 
-echo "SonarQube '$SONAR_VERSION-SNAPSHOT' installed!"
+echo "Sonarqube '$SONAR_VERSION-SNAPSHOT' installed!"
--- sonarqube/bin/s-installSnapshotOfPlugin.sh
+++ sonarqube/bin/s-installSnapshotOfPlugin.sh
@@ -3,49 +3,79 @@
 # Exit on failure
 set -e
 
-PLUGIN=$1
-DEPLOY_FOLDER=$PLUGINS_DEV
+case "$1" in 
+    ""|-h|--h|*-help)
+        echo "Usage: `basename $0` [PLUGINS] [-o]"
+        echo "    PLUGINS list of plugin to install"
+        echo "    -o      prevent building the plugins"
+        exit 0
+        ;;
+esac
 
-echo "Building SonarQube $1"
-
-if [ "$2" = "-o" ]
-then
-  echo "(no build required)"
-else
-  cd $REPOS/sonar-$PLUGIN
-  git pull
-  mvn clean install -DskipTests
-fi
-echo ""
-
-installPluginInDeployFolder() {
-  if [ "$2" = "" ]
-  then 
-    # Simple plugin
-    NAME=$1
-    BINARY="$REPOS/sonar-$PLUGIN/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar"
-  else
-  	# Multi-JAR plugin
-    NAME=$2
-  	BINARY="$REPOS/sonar-$PLUGIN/sonar-$2-plugin/target/sonar-$2-plugin-*-SNAPSHOT.jar"
-  fi
+deployPlugin() {
+    if [ "$2" = "" ]
+    then 
+        # Simple plugin
+        NAME=$1
+        BINARY="$REPOS/sonar-$PLUGIN/target/sonar-$PLUGIN-plugin-*-SNAPSHOT.jar"
+    else
+        # Multi-JAR plugin
+        NAME=$2
+        BINARY="$REPOS/sonar-$PLUGIN/sonar-$2-plugin/target/sonar-$2-plugin-*-SNAPSHOT.jar"
+    fi
   
-  echo "... Installing $NAME"
-  rm -f $DEPLOY_FOLDER/sonar-$NAME-plugin-*.jar
-  cp $BINARY $DEPLOY_FOLDER
-  echo "$NAME installed"
+    echo "Deploy plugin '$NAME'"
+    rm -f $PLUGINS_DEV/sonar-$NAME-plugin-*.jar
+    cp $BINARY $PLUGINS_DEV
+    echo "$NAME deployed"
 }
 
-PLUGINS_LIST=`ls $REPOS/sonar-$PLUGIN | grep plugin | sed 's/sonar-\(.*\)-plugin/\1/' `
+installPlugin() {
+    PLUGIN=$1
+    echo "Install snapshot of plugin '$PLUGIN'"
 
-if [ "$PLUGINS_LIST" = "" ]
-then
-  # Simple plugin
-  installPluginInDeployFolder $PLUGIN
-else
-  # Multi-JAR plugin
-  for p in $PLUGINS_LIST; do
-    installPluginInDeployFolder $PLUGIN $p
-  done
-fi
+    if [ "$2" = "-o" ]
+    then
+        echo "No build required"
+    else
+        echo "Get latest version of plugin"
+        cd $REPOS/sonar-$PLUGIN
 
+        echo "Build (quiet mode)..."
+        git pull
+        mvn clean install -DskipTests -q
+    fi
+
+    PLUGINS_LIST=`ls $REPOS/sonar-$PLUGIN | grep plugin | sed 's/sonar-\(.*\)-plugin/\1/'`
+    if [ -z "$PLUGINS_LIST" ]
+    then
+        # Simple plugin
+        deployPlugin $PLUGIN
+    else
+        # Multi-JAR plugin
+        for p in $PLUGINS_LIST
+        do
+            deployPlugin $PLUGIN $p
+        done
+    fi
+}
+
+#Check if build is required for plugins
+PLUGIN_BUILD_OPTION=""
+for var in "$@"
+do
+    if [ "$var" = "-o" ]
+    then
+        echo "No build required for plugins"
+        PLUGIN_BUILD_OPTION="-o"
+    fi
+done
+
+# Checks if plugins must be installed or not
+for var in "$@"
+do
+    if [ "$var" != "-o" ]
+    then
+        installPlugin $var $PLUGIN_BUILD_OPTION
+    fi
+done
