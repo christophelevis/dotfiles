659857a98923c417f9d1ea0eba8513eeec986131
Christophe Levis <christophe.levis@sonarsource.com>
2016-06-13 11:37:13 +0200

    SNAPSHOT and ARCHIVES folders, s-installPlugin command

--- sonarqube/bash/01-sonarqube.bashrc
+++ sonarqube/bash/01-sonarqube.bashrc
@@ -7,13 +7,13 @@ export PATH=$DOTFILES/sonarqube/bin:$PATH
 # Settings specific to test snapshot version of SonarQube
 # ================================
 export SONAR_CURRENT=$SOFTWARE_FOLDER/SonarQube/current
-export SONAR_NEXT_FILES=$SOFTWARE_FOLDER/SonarQube/NEXT_VERSION_FILES
+export SONAR_NEXT_FILES=$SOFTWARE_FOLDER/SonarQube/SNAPSHOT
 export SONAR_NEXT=$SOFTWARE_FOLDER/SonarQube/sonarqube-next
 export PLUGINS_DEV=$SONAR_CURRENT/extensions/plugins
 alias s-next="cd $SONAR_NEXT"
 
 export SONAR_SCANNER_CURRENT=$SOFTWARE_FOLDER/SonarScanner/current
-export SONAR_SCANNER_NEXT_FILES=$SOFTWARE_FOLDER/SonarScanner/NEXT_VERSION_FILES
+export SONAR_SCANNER_NEXT_FILES=$SOFTWARE_FOLDER/SonarScanner/SNAPSHOT
 export SONAR_SCANNER_NEXT=$SOFTWARE_FOLDER/SonarScanner/sonar-scanner-next
 
 export PATH=$SONAR_SCANNER_CURRENT/bin:$PATH
@@ -53,7 +53,7 @@ complete -F _find_install_ s-switch.sh
 # Find archives of sonarqube
 _find_archived_versions_()
 {
-    for file in ~/Software/SonarQube/*
+    for file in ~/Software/SonarQube/ARCHIVES/*
     do
 	if [ -f "$file" ]
         then
@@ -77,7 +77,7 @@ _find_archive_()
 }
 complete -F _find_archive_ s-install.sh
 
-# Find installed versions of sonarscanner
+# Find installed versions of scanner
 _find_installed_scanner_versions_()
 {
     for dir in ~/Software/SonarScanner/*
@@ -109,12 +109,12 @@ complete -F _find_install_scanner_ s-switchScanner.sh
 # Find archives of scanner
 _find_archived_scanner_versions_()
 {
-    for file in ~/Software/SonarScanner/*
+    for file in ~/Software/SonarScanner/ARCHIVES/*
     do
 	if [ -f "$file" ]
         then
 	    case $file in
-                *sonar*-*.zip)
+                *sonar-scanner*-*.zip)
                     echo "$file" | sed 's/.*sonar-scanner[^-]*-//' | sed 's/.*cli[^-]*-//' | sed 's/\.zip//'
                     ;;
             esac
@@ -160,3 +160,34 @@ _find_plugins_()
 }
 complete -F _find_plugins_ s-installSnapshotOfPlugin.sh
 complete -F _find_plugins_ s-installSnapshot.sh
+
+# Find archives of plugin
+_find_archived_plugin_versions_()
+{
+    for file in ~/Software/SonarQube/ARCHIVES/*
+    do
+	if [ -f "$file" ]
+        then
+	    case $file in
+                *sonar-*-plugin-*.jar)
+                    echo "$file" | sed 's/.*sonar[^-]*-//' | sed 's/-plugin//' | sed 's/\.jar//' | sed 's/-/_/'
+                    ;;
+                *-extension-plugin-*.jar)
+                    echo "$file" | sed 's/.*\///' | sed 's/extension-plugin-//' | sed 's/\.jar//' | sed 's/-/_/'
+                    ;;
+            esac
+        fi
+    done
+}
+_find_archive_plugin_()
+{
+    local cur prev
+
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    COMPREPLY=( $(compgen -W "$(_find_archived_plugin_versions_)" -- $cur) )
+    return 0
+}
+complete -F _find_archive_plugin_ s-installPlugin.sh
+
---
+++ sonarqube/bin/s-installPlugin.sh
@@ -0,0 +1,120 @@
+#!/bin/sh
+
+# No exit on failure
+# set -e
+
+usage() {
+        echo "Usage: `basename $0` [PLUGIN_VERSION] | [PLUGIN] [VERSION]"
+        echo "    PLUGIN:"
+        echo "        java"
+        echo "        javascript"
+        echo "        ..."
+        echo "    VERSION:"
+        echo "        X.Y"
+        echo "        X.Y-build*"
+        exit 0
+}
+
+case "$1" in 
+    *_*)
+	PLUGIN=$(echo "$1" | sed 's/_.*//')
+	VERSION=$(echo "$1" | sed 's/.*_//')
+        ;;
+    ""|-h|--h|*-help)
+        usage
+        ;;
+    *)
+        case "$2" in 
+            ""|-h|--h|*-help)
+                usage
+                ;;
+        esac
+        PLUGIN=$1
+        VERSION=$2
+        ;;
+esac
+
+dlAndExtractStable() {
+    PLUGIN_NAME="sonar-$1-plugin"
+    echo "Trying $PLUGIN_NAME-$2 on bintray..." >&2
+    URL="https://sonarsource.bintray.com/Distribution/$PLUGIN_NAME/$PLUGIN_NAME-$2.jar"
+
+    HTTP_CODE=$(curl --write-out '%{http_code}' --silent --output /dev/null --head "$URL")
+    if [ ! "$HTTP_CODE" = "200" ]  
+    then
+        echo "error"
+        return
+    fi
+
+    echo "Dowloading $PLUGIN_NAME-$2..." >&2
+    curl -L -# -o "$INSTALL_PATH/ARCHIVES/$PLUGIN_NAME-$2.jar" "$URL"
+    cp $INSTALL_PATH/ARCHIVES/$PLUGIN_NAME-$2.jar $SONAR_CURRENT/extensions/plugins/
+    echo "Plugin '$PLUGIN_NAME-$2' copied" >&2
+    echo "done"
+}
+
+dlAndExtractBuild() {
+    PLUGIN_NAME="$1-extension-plugin"
+    DEPLOY_NAME="sonar-$1-plugin-$2"
+    echo "Trying $PLUGIN_NAME-$2 on repox..." >&2
+    URL="https://repox.sonarsource.com/sonarsource-public-releases/org/sonarsource/$1/$PLUGIN_NAME/$2/$PLUGIN_NAME-$2.jar"
+
+    HTTP_CODE=$(curl --write-out '%{http_code}' --silent --output /dev/null --head "$URL")
+    if [ ! "$HTTP_CODE" = "200" ]  
+    then
+        echo "error"
+        return
+    fi
+
+    echo "Dowloading $PLUGIN_NAME-$2..." >&2
+    curl -L -# -o "$INSTALL_PATH/ARCHIVES/$PLUGIN_NAME-$2.jar" "$URL"
+    cp $INSTALL_PATH/ARCHIVES/$PLUGIN_NAME-$2.jar $SONAR_CURRENT/extensions/plugins/$DEPLOY_NAME.jar
+    echo "Plugin '$DEPLOY_NAME' copied" >&2
+    echo "done"
+}
+
+echo "Install plugin"
+
+INSTALL_PATH=$SOFTWARE_FOLDER/SonarQube
+if [ -f "$SONAR_CURRENT/extensions/plugins/sonar-$PLUGIN-plugin-$VERSION.jar" ]
+then
+    echo "Version already installed"
+else
+
+    if [ -f $INSTALL_PATH/ARCHIVES/sonar-$PLUGIN-plugin-$VERSION.jar ]
+    then
+        echo "Local copy 'sonar-$PLUGIN-plugin-$VERSION.jar' found"
+        cp $INSTALL_PATH/ARCHIVES/sonar-$PLUGIN-plugin-$VERSION.jar $SONAR_CURRENT/extensions/plugins/
+        echo "Plugin copied"
+    else
+        if [ -f $INSTALL_PATH/ARCHIVES/$PLUGIN-extension-plugin-$VERSION.jar ]
+        then
+            echo "Local copy '$PLUGIN-extension-plugin-$VERSION' found"
+            cp $INSTALL_PATH/ARCHIVES/$PLUGIN-extension-plugin-$VERSION.jar $SONAR_CURRENT/extensions/plugins/sonar-$PLUGIN-plugin-$VERSION.jar
+            echo "Plugin copied"
+        else
+
+            case "$VERSION" in 
+            *build*)
+                ret=$(dlAndExtractBuild $PLUGIN $VERSION)
+                if [ ! "$ret" = "done" ]
+                then
+                    echo "Build not found!"
+                    exit 1
+                fi
+                ;;
+    
+            *)
+                ret=$(dlAndExtractStable $PLUGIN $VERSION)
+                if [ ! "$ret" = "done" ]
+                then
+                    echo "Version not found!"
+                    exit 1
+                fi
+                ;;
+            esac
+
+        fi
+    fi
+
+fi
