cf9152d09ac53e00255f291063bc5e02e8091771
Christophe Levis <christophe.levis@sonarsource.com>
2016-09-21 18:21:00 +0200

    Update WS calls

--- sonarqube/bin/s-setLicenses.sh
+++ sonarqube/bin/s-setLicenses.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+setLicenseOldProduct() {
+  LICENSE=`more $REPOS/licenses/it/$1.txt | tr -d '\r\n'`
+  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
+}
 setLicense() {
   LICENSE=`more $REPOS/licenses/it/$1.txt | tr -d '\r\n'`
-  # double curl because all the plugins do not have the same property...
-  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
   curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
 }
 
-setLicense "views"
-setLicense "sqale"
+setLicenseOldProduct "views"
+setLicenseOldProduct "sqale"
+setLicenseOldProduct "report"
 setLicense "devcockpit"
-setLicense "report"
 setLicense "governance"
 
 # some language plugins too
@@ -19,10 +21,10 @@ setLicense "cpp"
 # -----------------------------------------------------------------------------
 # OLD WAY TO GET LICENSES
 # Need to have $SONARSOURCE_PLUGIN_LICENSES_URL defined as environment variable
-setLicenseOLD() {
-  LICENSE=`curl -s $SONARSOURCE_PLUGIN_LICENSES_URL/$1.txt | tr -d '\r\n'`
-  # double curl because all the plugins do not have the same property...
-  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
-  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
-}
+# setLicenseOLD() {
+#   LICENSE=`curl -s $SONARSOURCE_PLUGIN_LICENSES_URL/$1.txt | tr -d '\r\n'`
+#   # double curl because all the plugins do not have the same property...
+#  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=$1.license.secured&value=$LICENSE"
+#  curl -s -u admin:admin -X POST "http://localhost:9000/api/properties?id=sonar.$1.license.secured&value=$LICENSE"
+#}
 # -----------------------------------------------------------------------------
