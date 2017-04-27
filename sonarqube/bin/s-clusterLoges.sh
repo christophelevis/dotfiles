0b9f8707ca96019f3e3bd8929bb139bc7e9c580e
Christophe Levis <christophe.levis@sonarsource.com>
2017-04-27 18:01:30 +0200

    Add cluster scripts for logs and configuration

---
+++ sonarqube/bin/s-clusterLoges.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+usage() {
+    echo "Usage: `basename $0` [1|2|3]"
+    exit 0
+}
+
+case "$1" in
+	1|2|3)
+		tail -f $(readlink $SONAR_CURRENT)-NODE$1/logs/es.log
+		;;
+	*)
+		usage
+		;;
+esac
